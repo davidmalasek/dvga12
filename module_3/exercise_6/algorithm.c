24e37b911a7aa69eb5459ee63931a6b354bfbeb2
David Malášek <davidmalasek@post.cz>
2025-11-21 17:08:27 +0100

    Work on exercise 6

--- module_3/exercise_6/algorithm.c
+++ module_3/exercise_6/algorithm.c
@@ -7,17 +7,46 @@
 //
 // Here, you might want to add some private functions that get called by
 // public functions below.
+void swap(int *a, int *b) {
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
 //
 // Public
 //
 void bubble_sort(int *a, int n)
 {
-	printf("Bubble sort - To be implemented\n");
+	int i = 0;
+	int e = 0;
+	
+	int swapped = 0;
+
+	while (i < n - 1) {
+		e = 0;
+		swapped = 0;
+		while (e < n - 1 - i) { // - i because the elements on the right will be always sorted
+			if (a[e] > a[e + 1]) {
+				swap(&a[e], &a[e + 1]);
+				swapped = 1;
+			}
+			e++;
+		}
+		i++;
+		if (swapped == 0) // No swap happened, the array is sorted
+			break;
+	}
 }
 
 void insertion_sort(int *a, int n)
 {
-	printf("Insertion sort - To be implemented\n");
+	int i = 1;
+
+	while (i < n) {
+		// if (a[])
+	}
 }
 
 void quick_sort(int *a, int n)
